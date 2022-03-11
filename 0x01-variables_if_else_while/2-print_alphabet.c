6cfe618f0fddc2676170bbc8e984ca9f5f19ba37
VictorBK <victorbrian4@gmail.com>
2022-03-11 23:30:01 +0300

    Commit message for print alphabet task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase, followed by a new line.
+ * The only function to be used is putchar
+ * Return: 0
+ */
+int main(void)
+{
+int 1 = 'a';
+while (1 <= 'z')
+{
+putchar(1);
+1 += 1;
+}
+putchar('\n');
+return (0);
+}
