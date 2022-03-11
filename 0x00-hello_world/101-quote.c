54d101a9e1bc175cd2c60d98262d249605ef513f
VictorBK <victorbrian4@gmail.com>
2022-03-11 09:36:53 +0300

    Commit message for Quote task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *
- *Return: Always 0 (Success)
+ *Return: 1
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n"));
+Return (1);
+write(2,
+      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+      s);
 return (1);
 }
