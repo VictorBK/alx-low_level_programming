c51f7f59d6967757e25d480c67ee39e8b46a0b91
VictorBK <victorbrian4@gmail.com>
2022-03-12 11:32:41 +0300

    Commit message for print alphabets task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,14 +10,14 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
-
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
+}
+
+for (upper_case = 'A'; lower_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
 }
 
 putchar('\n');
