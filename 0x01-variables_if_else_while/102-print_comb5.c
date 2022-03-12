a0fef171f2fcd127a3e9874e3987d754e5336f50
VictorBK <victorbrian4@gmail.com>
2022-03-12 17:24:24 +0300

    Commit message for print comb5 task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - program to print all possible combinations of two 2-digit numbers
+ * only use putchar 8 times
+ * Return 0
+ */
+int main(void)
+{
+int c1 = 48;
+int x = 0;
+int y;
+int com = 44;
+
+while (x <= 99)
+{
+y = x + 1;
+while (y <= 99)
+{
+putchar((x / 10) + c1);
+putchar((x % 10) + c1);
+putchar(32);
+putchar((y / 10) + c1);
+putchar((y % 10) + c1);
+if (x != 98 || y != 99)
+{
+putchar(com);
+putchar(32);
+}
+y += 1;
+}
+x += 1;
+}
+putchar('\n');
+return (0);
+}
