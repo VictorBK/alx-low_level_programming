0b5600ab9bfcf3010fbd43a1c32fd2108ffce4d1
VictorBK <victorbrian4@gmail.com>
2022-03-12 16:34:45 +0300

    Commit message for print comb3 task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program for all possible different combinations of two digits
+ * 
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int d1 = 48;
+int d2;
+int com = 44;
+while (d1 <= 57)
+{
+d2 = d1 + 1;
+while (d2 <= 57)
+{
+putchar(d1);
+putchar(d2);
+if (d1 != 56 || d2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+d2 += 1;
+}
+d1 += 1;
+}
+putchar('\n');
+
+return (0);
+}
