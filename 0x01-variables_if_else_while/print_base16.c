cc2ce413c750ca3b95a9e2df192c942bc7d4d07a
VictorBK <victorbrian4@gmail.com>
2022-03-12 15:16:01 +0300

    Commit message for print base16 assignment

---
+++ 0x01-variables_if_else_while/print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints all numbers of base 16 in lowercase
+ * only use putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+int p = 48;
+int r = 'a';
+while (n <= 57)
+{
