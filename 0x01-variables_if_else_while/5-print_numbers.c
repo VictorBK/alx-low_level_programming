744cfba8d295a8a331d0a5a3667d3c1f2e8ab60e
VictorBK <victorbrian4@gmail.com>
2022-03-12 13:57:34 +0300

    Commit message for print digits task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10 starting from 0
+ * All code to be in the main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int x:
+
+for (x = 0 ; x < 10; x++)
+{
+printf("%x", x);
+}
+
+printf('\n');
+
+return (0);
+
+}
