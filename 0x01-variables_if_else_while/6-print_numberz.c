bca14e77ce87ffc8d2bdd57d3e4071aed261ef16
VictorBK <victorbrian4@gmail.com>
2022-03-12 14:24:46 +0300

    Commit message for print numberz task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program for all single digit numbers of base 10
+ * Do not use char
+ * Only use putchar
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int y;
+
+for (y = 48 ; y <= 57; i++)
+{
+putchar (y);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
