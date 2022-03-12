5df5a510f73da94f6564dcb35fd983962a0d4dbb
VictorBK <victorbrian4@gmail.com>
2022-03-12 14:37:36 +0300

    Commit message for print tebahpla task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program that prints the lowercase alphabet in reverse followed by a new line
+ * main - program that prints the lowercase alphabet in reverse
  * only use putchar
  * Return: 0
  */
