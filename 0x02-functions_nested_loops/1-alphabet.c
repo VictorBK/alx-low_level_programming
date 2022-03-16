b9f7161813162d0b789baa3dff1d7580821c1358
VictorBK <victorbrian4@gmail.com>
2022-03-16 05:27:34 +0300

    Commit for alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print the alphabet, lowercase
+ * Only use _putchar twice in your code
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(l);
+		x += 1;
+	}
+	_putchar(10);
+}
