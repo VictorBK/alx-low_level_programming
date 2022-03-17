d06b41b9575d86b6dde35dd1ae43ffa7d773d1e0
VictorBK <victorbrian4@gmail.com>
2022-03-17 11:13:16 +0300

    Commit message for multiple mains task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - program to test function that prints if integer is positive or negative
+ * main - main
  * Return: 0
  */
 
 int main(void)
 {
 int i;
-
 i = 0;
+
 positive_or_negative(i);
 
 return (0);
+
 }
