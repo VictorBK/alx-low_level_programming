3233be6a92fb605e68be1e2e91c51265065667b1
VictorBK <victorbrian4@gmail.com>
2022-03-21 14:23:36 +0300

    Commit message for a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to a parameter and updates its value.
+ * @n: pointer to an int.
+ * Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
