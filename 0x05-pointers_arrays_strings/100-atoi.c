1a0baa734e50c906410635e5a363a2fd769e30bc
VictorBK <victorbrian4@gmail.com>
2022-03-22 01:45:37 +0300

    Commit message for  a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,25 +9,23 @@
 int _atoi(char *s)
 {
 int x;
-int y;
-int z;
+int y, z;
 
-x = 0;
+y = 0;
 z = -1;
 for (x = 0; s[x] != '\0'; x++)
 {
-
 if (s[x] == '-')
 z *= -1;
 
-if (s[x] > 0 && s[x] < 9)
+if (s[x] > 47 && s[x] < 58)
 {
 if (y < 0)
 y = (y * 10) - (s[x] - '0');
 else
 y = (s[x] - '0') * -1;
 
-if (s[x + 1] < 0 || s[x + 1] > 9)
+if (s[x + 1] < 48 || s[x + 1] > 57)
 break;
 }
 }
