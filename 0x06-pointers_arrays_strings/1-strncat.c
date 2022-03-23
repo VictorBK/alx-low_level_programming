7f3545c78953207f448cfcad2f7c0e6a2f963e26
VictorBK <victorbrian4@gmail.com>
2022-03-23 13:37:36 +0300

    Commit message for strncat task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concantenates two strings
+ * @dest: destination string to be concantenated to
+ * @src: source string to be concantenated to dest
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int x = 0;
+int y = 0;
+
+while (*(dest + x) != '\0')
+{
+y++;
+x++;
+}
+
+x = 0;
+while (*(src + x) != *(src + n))
+{
+*(dest + y) = *(src + x);
+x++;
+y++;
+}
+*(dest + y) = '\0';
+return (dest);
+}
