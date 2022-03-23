5421b7ee5c291408928a5c54497b3b1d97d8fb6f
VictorBK <victorbrian4@gmail.com>
2022-03-24 01:12:59 +0300

    Commit message for a function that concantenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,36 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - function that concantenates two strings
- * @dest: destination string to be concantenated to
- * @src: source string to be concantenated to dest
- *
- * Return: dest
+ *_strncat - function that concantenates two strings
+ *@dest: pointer to character to be changed
+ *@src: pointer to character to be changed
+ *@n: value
+ *Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
-{
 
+{
 int x,y;
 
 x = 0;
+while (dest[x] != '\0')
+{
+x++;
+}
+
 y = 0;
-
-while (*(dest + x) != '\0')
+while (y < n && src[y] != '\0')
 {
+dest[x] = src[y];
 y++;
 x++;
 }
 
-x = 0;
-while (*(src + x) != *(src + n))
-{
-*(dest + y) = *(src + x);
-x++;
-y++;
-
-}
-*(dest + y) = '\0';
+dest[x] = '\0';
 
 return (dest);
 }
