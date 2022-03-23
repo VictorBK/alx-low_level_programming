4d120126fc78224ba4175ebcd0d7edd490160e7c
VictorBK <victorbrian4@gmail.com>
2022-03-23 22:14:38 +0300

    Commit message for a function that compares two strings and works exactly like strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
 /**
- * _strcmp - function to compare two strings
- * function should work exactly like strcmp
- * @s1: string to compare to s2
- * @s2: string to compare to s1
- *
+ * _strcmp - function that compares two strings.
+ * @s1: first input string
+ * @s2: second input string
  * Return: difference between the strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int a;
-int output;
-
-a = 0;
-
-while (*(s1 + a) == *(s2 + a) && *(s1 + a) != '\0')
-a++;
-
-output = (*(s1 + a) - *(s2 + a));
-
-return (output);
+while (*s1 && *s2)
+{
+if (*s1 != *s2)
+return (*s1 - *s2);
+s1++, s2++;
+}
+return (*s1 - *s2);
 }
