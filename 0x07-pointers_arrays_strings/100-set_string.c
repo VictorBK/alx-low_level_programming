5a8d9f77c4f90e78e035c9d14bc7bd7b0fc999a8
VictorBK <victorbrian4@gmail.com>
2022-03-29 00:51:02 +0300

    Commit message for a function that sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function that sets value of a pointer to a char
+ * @s: variable type pointer
+ * @to: char
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
