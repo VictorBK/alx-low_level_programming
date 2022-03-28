9dbb0f43fe46c4d8985c96d5b98442e814270878
VictorBK <victorbrian4@gmail.com>
2022-03-28 12:58:07 +0300

    Commit message for a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of memory area
- * memory area is pointed to by s with constant byte b
- * @s: pointer that points memory area
- * @b: constant to be filled with memory
- * @n: bytes of memory area that is to be filled
+ * _memset - function that fills memory with a constant byte.
  *
- * Return: pointer to memory area s
+ * @s: memory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: char
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int x;
+	unsigned int i = 0;
 
-for (x = 0; x < n; x++)
-{
-*(s + x) = b;
-}
-return (s);
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
 }
