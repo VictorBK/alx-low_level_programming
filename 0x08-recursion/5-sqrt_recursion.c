276c4e2ab7ee0eb5e5ed04ea87cde41a7a096d2d
VictorBK <victorbrian4@gmail.com>
2022-03-30 13:58:29 +0300

    Commit message for a function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,25 @@
 #include "main.h"
 
 /**
+ * find_root - function that gets the num of square root
+ * @num: input number
+ * @c: counter variable
+ * Return: int
+ */
+
+int find_root(int num, int c)
+{
+if (num == (c * c))
+return (c);
+else if (num > (c * c))
+return (find_root(num, c + 1));
+else
+return (-1);
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
- * if n does not have a natural square root, return -1
+ * if n does not have natural square root, return -1
  * @n: input number
  * Return: int
  */
@@ -15,22 +32,5 @@ if (n == 1)
 return (1);
 if (n < 0)
 return (-1);
-return (getNumSqr(n, 1));
-}
-
-/**
- * getNumSqr - function that determines number of squareroot
- * @num: input number
- * @c: counter variable
- * Return: int
- */
-
-int getNumSqr(int num, int c)
-{
-if (num == (c * c))
-return (c);
-if (num > (c * c))
-return (getNumSqr(num, c + 1));
-else
-return (-1);
+return (find_root(n, 1));
 }
