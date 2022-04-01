94f727ca63872e79a2ddf4fde29ffae5887cd9af
VictorBK <victorbrian4@gmail.com>
2022-04-01 21:37:52 +0300

    Commit message for a program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print product of numbers in argument
+ * @argc: argument count
+ * @argv: an array of size argc
+ *
+ * Return: 0 (success)
+ * Return: 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+if (argv[1] && argv[2])
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+printf("Error\n");
+
+return (1);
+}
