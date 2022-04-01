e38328677d7cdc7e2a727856ebd4a08ce6113c0e
VictorBK <victorbrian4@gmail.com>
2022-04-01 20:45:45 +0300

    Commit message for a program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments that it receives
+ * @argc: argument count
+ * @argv: an array of size argc
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
