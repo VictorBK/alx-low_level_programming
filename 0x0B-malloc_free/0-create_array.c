4185b1c5afa79cb6273ad50f7eb099a2ca1f2d66
VictorBK <victorbrian4@gmail.com>
2022-04-05 02:28:13 +0300

    Commit message for a function that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char
+ * @size: size pf array
+ * @c: specific character
+ * Return: pointer, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int n;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(size * sizeof(*arr));
+	if (arr == NULL)
+		return (NULL);
+
+	for (n = 0; n < size; n++)
+		arr[n] = c;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+My readme for the malloc_free directory/project
