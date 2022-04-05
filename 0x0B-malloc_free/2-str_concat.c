26ed35012cb8a6e834bd556971e42cc1e808416e
VictorBK <victorbrian4@gmail.com>
2022-04-05 10:44:19 +0300

    Commit message for a function that concantenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,31 +14,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-        char *a;
-        char *nul = "";
-        unsigned int w, x, y, z;
-
-        if (s1 == NULL)
-                s1 = nul;
-        if (s2 == NULL)
-                s2 = nul;
-
-        for (w = 0; s1[w] != '\0'; w++)
-                ;
-        for (x = 0; s2[x] != '\0'; x++)
-                ;
-
-        a = malloc((w * sizeof(*s1)) + (x * sizeof(*s2)) + 1);
-        if (a == NULL)
-                return (NULL);
-
-        for (y = 0, z = 0; y < (w + x + 1); y++)
-        {
-                if (y < w)
+	char *a;
+	char *nul = "";
+	unsigned int w, x, y, z;
+	
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
+	
+	for (w = 0; s1[w] != '\0'; w++)
+		;
+	for (x = 0; s2[x] != '\0'; x++)
+		;
+	
+	a = malloc((w * sizeof(*s1)) + (x * sizeof(*s2)) + 1);
+	if (a == NULL)
+		return (NULL);
+	
+	for (y = 0, z = 0; y < (w + x + 1); y++)
+	{
+		if (y < w)
 			a[y] = s1[y];
 		else
 			a[y] = s2[z++];
 	}
-
 	return (a);
 }
