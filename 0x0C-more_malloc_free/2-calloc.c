027690234a7c08686213aeb9dd6f4a9fb17603b1
VictorBK <victorbrian4@gmail.com>
2022-04-08 00:39:44 +0300

    Commit message for a function that allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of size bytes each 
+ * @nmemb: number of elements
+ * @size: byte size of each element of array
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int n;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (n = 0; n < nmemb * size; n++)
+		p[n] = 0;
+
+	return (p);
+}
