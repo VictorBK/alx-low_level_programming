35159796e1c2207bb7edc8edc88ca579d7cf6cbe
VictorBK <victorbrian4@gmail.com>
2022-04-12 00:46:37 +0300

    Commit messaage for a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - function to create new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	unsigned int a = 0, b = 0, count;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	while (name[a] != '\0')
+		a++;
+	a++;
+	(*d).name = malloc(sizeof(char) * a);
+	if((*d).name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (count = 0; count < a; count++)
+		(*d).name[count] = name[count];
+
+	(*d).age = age;
+
+	while (owner[b] != '\0')
+		b++;
+	b++;
+	(*d).owner = malloc(sizeof(char) * b);
+	if ((*d).owner == NULL)
+	{
+		free((*d).name);
+		free(d);
+		return (NULL);
+	}
+	for (count = 0; count < b; count++)
+		(*d).owner[count] = owner[count];
+
+	return (d);
+}
