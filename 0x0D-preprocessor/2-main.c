b083f5b6d17c61956f45e081578af82198f88b9d
VictorBK <victorbrian4@gmail.com>
2022-04-11 22:38:09 +0300

    Commit message for a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints current filename to std output
+ * 
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
