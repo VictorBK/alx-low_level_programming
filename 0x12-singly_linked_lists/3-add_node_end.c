d4ce6b08c34b247ee4074da78a559df019e27696
VictorBK <victorbrian4@gmail.com>
2022-04-22 01:50:56 +0300

    Write a function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,18 +26,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
-	
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
 
-	while (temp -> next)
+	while (temp->next)
 		temp = temp->next;
 
 	temp->next = new;
 
 	return (new);
 }
-
