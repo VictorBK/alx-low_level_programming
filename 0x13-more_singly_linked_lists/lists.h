203507157e6d489eb833cde9fe3e6fd918804070
VictorBK <victorbrian4@gmail.com>
2022-04-25 22:41:21 +0300

    Write a function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
-#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -33,5 +32,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* #ifndef _LISTS_H_ */
 
